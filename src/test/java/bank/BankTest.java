06c6cc508a20f51410fbd94b383c6056059554a8
2025MEMES\jamie <jamiewillis200246@gmail.com>
2025-10-26 19:07:52 +0800

    Add test to Bank class, bug fix in bank class

---
+++ src/test/java/bank/BankTest.java
@@ -0,0 +1,80 @@
+package bank;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import user.User;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static utils.Currency.SGD;
+import static utils.Currency.JPY;
+import static utils.Currency.CNY;
+
+public class BankTest {
+    private static final Path TX_FILE = Path.of("transactions.txt");
+    @BeforeEach
+    public void setUp() throws IOException {
+        Files.deleteIfExists(TX_FILE);
+        User.initialise();
+        Bank b1 = new Bank(0, SGD, 100, 0.4f);
+        Bank b2 = new Bank(0, JPY, 1000, 2f);
+        User.addBank(b1);
+        User.addBank(b2);
+        User.currBank = b2;
+
+    }
+
+    @Test
+    public void bank_setBalance_success(){
+        float b1 = User.currBank.getBalance();
+        User.currBank.setBalance(110);
+        float b2 = User.currBank.getBalance();
+        assertEquals(110, b2);
+    }
+
+    @Test
+    public void bank_setNegativeBalance_exceptionThrown(){
+        float b1 = User.currBank.getBalance();
+        assertThrows(IllegalArgumentException.class, () -> {
+            User.currBank.setBalance(-10);
+        });
+        float b2 = User.currBank.getBalance();
+        assertEquals(b1, b2);
+    }
+
+    @Test
+    public void bank_initialiseNegativeBalance_exceptionThrown(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Bank(0, CNY, -100, 0.2f);
+        });
+    }
+
+    @Test
+    public void bank_setExchangeRate_success(){
+        float b1 = User.currBank.getExchangeRate();
+        User.currBank.setExchangeRate(0.5f);
+        float b2 = User.currBank.getExchangeRate();
+        assertEquals(0.5f, b2);
+    }
+
+    @Test
+    public void bank_setNegativeExchangeRate_exceptionThrown(){
+        float b1 = User.currBank.getExchangeRate();
+        assertThrows(IllegalArgumentException.class, () -> {
+            User.currBank.setExchangeRate(-0.3f);
+        });
+        float b2 = User.currBank.getExchangeRate();
+        assertEquals(b1, b2);
+    }
+
+    @Test
+    public void bank_initialiseNegativeExchangeRate_exceptionThrown(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Bank(0, CNY, 0, -0.2f);
+        });
+    }
+}
