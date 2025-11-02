430c4d0fba4495879df7f95a606d69c650f64ed2
2025MEMES\jamie <jamiewillis200246@gmail.com>
2025-11-02 16:05:45 +0800

    fix-ci

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -7,8 +7,8 @@ import static ui.Parser.parseCommand;
 
 public class UserInterface {
     public static void main(String[] args) {
-        System.out.println("Warning: If '⚠️'(hazard symbol) looks like '??'(two question marks) " +
-                "then your console encoding is not UTF-8, some symbols (e.g. '฿') may not display correctly. " +
+        System.out.println("Warning: If some symbols not displaiying properly then your console encoding is" +
+                "not UTF-8, some symbols (e.g. '฿') may not display correctly. " +
                 "Consider running 'chcp 65001' in cmd before opening this program to ensure " +
                 "proper encoding and display.");
         User.initialise();
